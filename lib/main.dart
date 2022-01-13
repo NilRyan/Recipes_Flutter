e45b564b70c7b0f71b5240f0f39f6943b61657c9
Neil Ryan L <neilryanlipaod@gmail.com>
2022-01-13 16:31:34 +0800

    Add Card widget for rendering recipe

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
+import 'package:recipes/recipe.dart';
 
 void main() {
   runApp(const RecipeApp());
@@ -41,7 +42,41 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: SafeArea(
-        child: Container(),
+        child: ListView.builder(
+          itemCount: Recipe.samples.length,
+          itemBuilder: (BuildContext context, int index) {
+            // TODO: Update to return Recipe card
+            return buildRecipeCard(Recipe.samples[index]);
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget buildRecipeCard(Recipe recipe) {
+    return Card(
+      elevation: 2.0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0)
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+        children: <Widget>[
+          Image(image: AssetImage(recipe.imageUrl)),
+          const SizedBox(
+            height: 14.0,
+          ),
+          Text(
+            recipe.label,
+            style: const TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.w700,
+              fontFamily: 'Palatino',
+            )
+          ),
+        ],
+      ),
       ),
     );
   }
