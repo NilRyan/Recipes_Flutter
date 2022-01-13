91e25a8cd501288f72f221a0b94cd740d1a4c3f9
Neil Ryan L <neilryanlipaod@gmail.com>
2022-01-13 16:46:47 +0800

    Add route for details page

--- lib/main.dart
+++ lib/main.dart
@@ -45,9 +45,16 @@ class _MyHomePageState extends State<MyHomePage> {
         child: ListView.builder(
           itemCount: Recipe.samples.length,
           itemBuilder: (BuildContext context, int index) {
-            // TODO: Update to return Recipe card
-            return buildRecipeCard(Recipe.samples[index]);
-          },
+            return GestureDetector(
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  // TODO: Replace return with return RecipeDetail()
+                  return const Text('Detail Page');
+                }));
+              },
+              child: buildRecipeCard(Recipe.samples[index]),
+            );
+            },
         ),
       ),
     );
@@ -56,27 +63,23 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget buildRecipeCard(Recipe recipe) {
     return Card(
       elevation: 2.0,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10.0)
-      ),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
       child: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
-        children: <Widget>[
-          Image(image: AssetImage(recipe.imageUrl)),
-          const SizedBox(
-            height: 14.0,
-          ),
-          Text(
-            recipe.label,
-            style: const TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.w700,
-              fontFamily: 'Palatino',
-            )
-          ),
-        ],
-      ),
+          children: <Widget>[
+            Image(image: AssetImage(recipe.imageUrl)),
+            const SizedBox(
+              height: 14.0,
+            ),
+            Text(recipe.label,
+                style: const TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w500,
+                  fontFamily: 'Palatino',
+                )),
+          ],
+        ),
       ),
     );
   }
